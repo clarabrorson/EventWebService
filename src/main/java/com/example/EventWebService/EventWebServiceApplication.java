059f76f387846ee4292038a3846ca6ce5ad75f63
clarabrorson <88clabro@gafe.molndal.se>
2024-02-19 13:06:27 +0100

    Initial commit

---
+++ src/main/java/com/example/EventWebService/EventWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventWebService/EventWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
